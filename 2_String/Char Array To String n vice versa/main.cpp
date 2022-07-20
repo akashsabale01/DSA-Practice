80d050cc34b530fb2ee22b80ff27cd17cae140d3
Akash Sabale <akashsabale100@gmail.com>
2022-07-20 13:21:40 +0530

    added files

--- 2_String/Char Array To String n vice versa/main.cpp
+++ 2_String/Char Array To String n vice versa/main.cpp
@@ -1,5 +1,7 @@
+// https://www.geeksforgeeks.org/convert-string-char-array-cpp/
+
 #include <iostream>
-#include <cstring>
+#include <string.h>
 
 using namespace std;
 
@@ -15,44 +17,54 @@ bool isVowel(char ch)
     return false;
 }
 
-// char *remove_vowels(char *word)
-// {
-//     string temp = "";
-//     int wordSize = strlen(word);
-//     for (int i = 0; i < wordSize; i++)
-//     {
-//         if (isVowel(word[i]))
-//             temp += word[i];
-//     }
-
-//     char *res = &temp[0];
-//     return res;
-// }
-
-string removeVowels(string word)
+char *removeVowels(char *word)
 {
-    string res = "";
-    int wordSize = word.size();
+    string temp = "";
+    int wordSize = strlen(word);
+
+    // Remove Vowels & store it in temp string
     for (int i = 0; i < wordSize; i++)
     {
         if (!isVowel(word[i]))
-            res += word[i];
+            temp += word[i];
     }
 
-    return res;
+    // Convert string to char array(i.e. char pointer)
+    int tmpSize = temp.size();
+    char *result = new char[tmpSize];
+
+    // Copy elements from temp string to result char array
+    for (int i = 0; i < tmpSize; i++)
+        result[i] = temp[i];
+
+    result[tmpSize] = '\0'; // Last char as of resultult
+
+    return result;
 }
+
+// Method 2 - Using String
+
+// string removeVowels(string word)
+// {
+//     string res = "";
+//     int wordSize = word.size();
+//     for (int i = 0; i < wordSize; i++)
+//     {
+//         if (!isVowel(word[i]))
+//             res += word[i];
+//     }
+//     return res;
+// }
 
 int main()
 {
 
-    // char *str = "Hello";
+    char *str = "Hello";
 
-    // char *result = remove_vowels(str);
+    char *result = removeVowels(str);
 
-    // for (int i = 0; i != '\0'; i++)
-    //     cout << result[i] << " ";
-
-    string result = removeVowels("babylon");
+    for (int i = 0; i != '\0'; i++)
+        cout << result[i] << " ";
 
     cout << result;
 
